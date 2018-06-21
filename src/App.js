90741f8af8c5e408035ab2b7455f7d3f8ab67713
MikeWang <hi.from.mikewang@gmail.com>
2018-06-21 19:27:02 +1200

    Add examples for Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Example3 from './3_rendering_elements/Example3'
 import Example4 from './4_components_and_pros/Example4'
 import Example5 from './5_state_and_lifecycle/Example5'
 import Example6 from './6_handling_events/Example6'
+import Example7 from './7_conditional_rendering/Example7'
 
 class App extends Component {
 
@@ -20,20 +21,23 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-          <h1>example2:</h1>
+          <h1 className="App-example">example2:</h1>
           <Example2/>
 
-          <h1>example3:</h1>
+          <h1 className="App-example">example3:</h1>
           <Example3/>
 
-          <h1>example4:</h1>
+          <h1 className="App-example">example4:</h1>
           <Example4/>
 
-          <h1>example5:</h1>
+          <h1 className="App-example">example5:</h1>
           <Example5/>
 
-          <h1>example6:</h1>
+          <h1 className="App-example">example6:</h1>
           <Example6/>
+
+          <h1 className="App-example">example7:</h1>
+          <Example7/>
 
       </div>
     );
